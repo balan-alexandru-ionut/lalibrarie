b1fa9a0ef6a97bb841032e572dfeb48d95886ebf
Alexandru Ionut Balan <balan.alexandru1997@icloud.com>
2025-04-28 19:28:37 +0300

    Add new tea ingredients and update tea recipes
    Expanded the ingredient list to include new entries such as rooibos, saffron, and kombucha powder. Updated existing tea recipes to incorporate the new ingredients, refined formatting, and added new tea varieties like Green Tea Chai and Red Bush Fire.

--- src/components/products/data/ingredients.ts
+++ src/components/products/data/ingredients.ts
@@ -1,12 +1,14 @@
 // tea-ingredients.ts
 export const BLACK_TEA = 'ceai negru'
 export const GREEN_TEA = 'ceai verde'
-export const WHITE_TEA = 'ceai alb';
+export const WHITE_TEA = 'ceai alb'
 export const FERMENTED_TEA = 'ceai semi-fermentat'
 export const CHINA_SENCHA = 'China Sencha'
 export const LUNG_CHING = 'Ling Ching'
 export const YUNNAN = 'Yunnan'
 export const PAI_MU_TAN = 'Pai Mu Tan'
+export const ROOIBOS = 'rooibos'
+export const GREEN_ROOIBOS = 'ceai verde rooibos'
 
 // Spices
 export const CINNAMON = 'bucăți de scorțișoară'
@@ -19,6 +21,9 @@ export const CORIANDER = 'coriandru'
 export const CLOVES = 'cuișoare'
 export const ANISE_SEEDS = 'semințe de anason'
 export const CARAWAY = 'chimen'
+export const NUTMEG = 'nucșoarǎ'
+export const SAFFRON = 'șofrǎnaș'
+export const PINK_PEPPER = 'boabe de piper negru'
 
 // Fruits and nuts
 export const ORANGE_PEEL = 'coajă de portocală'
@@ -28,23 +33,28 @@ export const ALMONDS = 'bucățele de migdale'
 export const ALMOND_SLICES = 'migdale feliate'
 export const HAZELNUTS = 'alune de pădure'
 export const PISTACHIOS = 'fistic'
-export const MANGO_PIECES = 'cuburi de mango';
-export const MANGO_FLAKES = 'fulgi de mango';
-export const PINEAPPLE_PIECES = 'cuburi de ananas';
-export const PINEAPPLE_FLAKES = 'fulgi de ananas';
-export const PAPAYA_PINEAPPLE_CUBES = "cuburi de papaya și ananas"
-export const DEHYDRATED_STRAWBERRY = "bucăți de căpșuni deshidratate prin înghețare"
-export const LYCHEE = "lychee"
-export const LEMON_PEEL = "coajǎ de lǎmâie"
-export const LEMON_GRASS = "lemon grass"
-export const LEMON_FLAVOR = "arome naturale de lǎmâie"
-
+export const MANGO_PIECES = 'cuburi de mango'
+export const MANGO_FLAKES = 'fulgi de mango'
+export const PINEAPPLE_PIECES = 'cuburi de ananas'
+export const PINEAPPLE_FLAKES = 'fulgi de ananas'
+export const PAPAYA_PINEAPPLE_CUBES = 'cuburi de papaya și ananas'
+export const DEHYDRATED_STRAWBERRY = 'bucăți de căpșuni deshidratate prin înghețare'
+export const LYCHEE = 'lychee'
+export const LEMON_PEEL = 'coajǎ de lǎmâie'
+export const LEMON_GRASS = 'lemon grass'
+export const LEMON_FLAVOR = 'arome naturale de lǎmâie'
+export const DEHYDRATED_CHERRIES = 'cireșe uscate prin înghețare'
+export const KOMBUCHA_POWDER = 'pudrǎ de Kombucha'
+export const POMEGRANATE_SEEDS = 'boabe de rodie'
+export const DEHYDRATED_RASPBERRY = 'fructe uscate întregi de zmeurǎ aromatǎ'
+export const SMASHED_HAZLENUTS = 'bucǎțele de alune sfǎrâmate'
 
 // Sweets
 export const VANILLA_PIECES = 'bucățele de vanilie'
 export const CHOCOLATE_FLAKES = 'fulgi de ciocolată'
 export const SUGAR_CUBES = 'bucǎți sfǎrâmate de cuburi de zahǎr'
-export const COCOA = 'bucǎți de cacao'
+export const COCOA_BITS = 'bucǎți de cacao'
+export const COCOA = 'cacao'
 export const DARK_CHOCOLATE = 'bucǎți de ciocolatǎ neagrǎ'
 export const WHITE_CHOCOLATE = 'bucǎți de ciocolatǎ albǎ'
 export const VANILLA_COCOA = 'cacao cu bucǎți de vanilie'
@@ -52,25 +62,30 @@ export const BOURBON_VANILLA = 'bucǎți de vanilie bourbon'
 export const VANILLA_FLAVOR = 'aromǎ naturalǎ de vanilie'
 export const COCONUT_FLAKES = 'fulgi de cocos'
 export const CANDIED_STRAWBERRY_SLICES = 'felii de cǎpșuni confiate'
-export const CARAMEL = "bucǎți de caramel"
-export const SUGAR = "zahǎr"
+export const CARAMEL = 'bucǎți de caramel'
+export const SUGAR = 'zahǎr'
+export const CANE_SUGAR = 'zahǎr de trestie'
+export const INVERTED_SUGAT = 'zahǎr invertit'
 
 // Flowers
-export const ROSES = 'flori de trandafir';
+export const ROSES = 'flori de trandafir'
 export const ROSE_BUDS = 'boboci de trandafiri'
 export const PINK_ROSE_BUDS = 'boboci de trandafiri roz'
-export const ROSE_PETALS = 'petale de trandafir';
-export const MALLOW = "flori de nalbǎ";
+export const ROSE_PETALS = 'petale de trandafir'
+export const MALLOW = 'flori de nalbǎ'
 export const SUNFLOWER_BUDS = 'boboci de floarea soarelui'
 export const SUNFLOWER_PETALS = 'petale de floarea soarelui'
 export const JASMINE_BUDS = 'boboci de iasomie'
 export const JASMINE = 'flori de iasomie'
-export const MARIGOLD = "flori de gǎlbenele"
-export const SUNFLOWER_INFLORESCENCE = "inflorescențǎ de floarea-soarelui"
+export const MARIGOLD = 'flori de gǎlbenele'
+export const SUNFLOWER_INFLORESCENCE = 'inflorescențǎ de floarea-soarelui'
+export const POMEGRANATE_FLOWERS = 'flori de rodie'
 
 // Plants
 export const CHAMOMILE = 'mușețel'
+export const MINT = 'mentǎ'
 
 // Other
 export const FLAVORS = 'arome'
 export const CHICORY_ROOTS = 'rădăcini de cicoare'
+export const ORANGE_FLAVORS = 'arome naturale de portocalǎ'
